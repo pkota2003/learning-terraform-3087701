0b101afbab9266890e92f35fc8ec33fda9d815fe
pkota2003 <64416937+pkota2003@users.noreply.github.com>
2026-02-25 14:57:44 -0500

    move attachment

--- main.tf
+++ main.tf
@@ -69,12 +69,7 @@ resource "aws_lb_target_group" "blog" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.blog_vpc.vpc_id
-}
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
 }
 
 
